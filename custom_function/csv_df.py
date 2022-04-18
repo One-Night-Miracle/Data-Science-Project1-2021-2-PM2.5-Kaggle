d21d126b5c38fab73a3784360cf4112624a4d9b6
shrria <58872002+shrria@users.noreply.github.com>
2022-04-18 12:57:15 +0700

    delete bfill and fix bugs

--- custom_function/csv_df.py
+++ custom_function/csv_df.py
@@ -46,7 +46,7 @@ def toDF(pm25_filename, temp_filename, wind_filename):
 
     return df
 
-def toDFtest(pm25_filename, temp_filename, wind_filename):
+def toDFtest(pm25_filename, temp_filename, wind_filename, temp_train_filename, wind_train_filename):
     pm25_df = pd.read_csv('datasci_dataset_2022/'+pm25_filename, names=['Time', 'PM25'], skiprows=1)
     pm25_df['Time'] = pd.to_datetime(pm25_df['Time'])
     pm25_df.set_index('Time', inplace=True)
@@ -55,22 +55,36 @@ def toDFtest(pm25_filename, temp_filename, wind_filename):
     pm25_df.interpolate(inplace=True)
     pm25_df.index = pd.DatetimeIndex(pm25_df.index)
 
+    temp_train_df = pd.read_csv('datasci_dataset_2022/'+temp_train_filename, names=['Time', 'Temp'], skiprows=1)
+    temp_train_df['Time'] = pd.to_datetime(temp_train_df['Time'])
     temp_df = pd.read_csv('datasci_dataset_2022/'+temp_filename, names=['Time', 'Temp'], skiprows=1)
     temp_df['Time'] = pd.to_datetime(temp_df['Time'])
+
+    temp_df = temp_df.append(temp_train_df.iloc[-1]).reset_index()
+    temp_df = temp_df.drop(columns=['index'])
+    temp_df = temp_df.sort_values(by=['Time'])
+
     temp_df.set_index(temp_df['Time'], inplace=True)
     temp_df.drop(columns={'Time'}, inplace=True)
     temp_df.columns = ['Temp']
     temp_df = temp_df.resample('h').ffill()
-    temp_df = temp_df.bfill()
+    # temp_df = temp_df.bfill()
 
+    wind_train_df = pd.read_csv('datasci_dataset_2022/'+wind_train_filename, names=['Time', 'WindSpeed', 'WindDir'], skiprows=1)
+    wind_train_df['Time'] = pd.to_datetime(wind_train_df['Time'])
     wind_df = pd.read_csv('datasci_dataset_2022/'+wind_filename, names=['Time', 'WindSpeed', 'WindDir'], skiprows=1)
     wind_df['Time'] = pd.to_datetime(wind_df['Time'])
+
+    wind_df = wind_df.append(wind_train_df.iloc[-1]).reset_index()
+    wind_df = wind_df.drop(columns=['index'])
+    wind_df = wind_df.sort_values(by=['Time'])
+    
     wind_df.set_index(wind_df['Time'], inplace=True)
     wind_df.drop(columns={'Time'}, inplace=True)
     wind_df.columns = ['WindSpeed', 'WindDir']
-    # backward filling
+    # forward filling
     wind_df = wind_df.resample('h').ffill()
-    wind_df = wind_df.bfill()
+    # wind_df = wind_df.bfill()
     # wind_df.index = pd.DatetimeIndex(wind_df.index)
 
     pm25_df['copy_index'] = pm25_df.index
@@ -81,6 +95,6 @@ def toDFtest(pm25_filename, temp_filename, wind_filename):
 
     # padding first and last indices
     df = df.ffill()
-    df = df.bfill()
+    # df = df.bfill()
 
     return df
